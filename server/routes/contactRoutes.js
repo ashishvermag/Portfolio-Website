5a27e24e580497dc12819cac30cad137ad3fba3e
Ashish Verma <ashish.av35@gmail.com>
2026-05-21 00:46:14 +0530

    Remove server folder from repository

--- server/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContact } = require('../controllers/contactController');
-
-// POST request to /api/contact
-router.post('/', submitContact);
-
-module.exports = router;
